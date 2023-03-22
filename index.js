d41aa52b224f9693dcb59c7bbe00d4fcd173ccae
AndrewLester <alester220@gmail.com>
2023-03-21 23:52:03 -0500

    Done

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-import AlgoliaHTMLExtractor from './lib/algoliaHtmlExtractor';
+import AlgoliaHTMLExtractor from './lib/algoliaHtmlExtractor.js';
 export default AlgoliaHTMLExtractor;
+const Extractor = new AlgoliaHTMLExtractor()
+let htmlFragments =await Extractor
+		// These are the top-level HTML elements that we keep - this results in a lot of fragments
+		.run(`<h1>Test</h1><p>fewf wf wfw fewuf ew fwuhefhw euf</p>`, { cssSelector: `p,pre,td,li` });
+console.log(htmlFragments);
